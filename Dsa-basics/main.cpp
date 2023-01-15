1a091ab024eda91cfa6e3526d3910f8dd1b24ef5
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2023-01-15 18:06:46 +0530

    add-semicolon

---
+++ Dsa-basics/main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int i=57;
+      i=++i;
+      cout <<"the no is pre  increment by one"<<i<<endl;
+      --i;
+      cout <<"the no is pre decremnt by one"<<i<<endl;
+      return 0;
+}
