c41496939c9f3810e4c92f540bda600505e75da1
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2023-01-16 23:44:34 +0530

    if else
    Co-authored-by: Tanooshri Vajpayee <tanooshrivajpayee06@gmail.com>

---
+++ Dsa-basics/array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=50;
+    if (n>0)
+    {
+        cout<<" n is positive";
+    }
+    
+    cout <<n;
+    cout<< "hello world";
+    return 0;
+}
