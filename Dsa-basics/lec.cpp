ceaf7bb9ffe9cfe05ec30e5e77099dc75e2f7a46
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2023-01-17 20:02:34 +0530

    lec.cpp

---
+++ Dsa-basics/lec.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main(){
+// int a=9; if (a == 9)
+//  {
+//  cout <<"NINEY";
+//  }
+//  if (a >0)
+//  {
+//     cout<<"positive";
+//  }
+//  else{
+//      cout <<"negative";
+//  }
+//      return 0;
+//  }
+// second:
+//   int a=2;
+//   int b=a+1;
+//  if ((a=3)==b)
+//   {
+//      cout <<a;
+
+//  }
+//   else{
+//       cout<< a+1;
+//   }
+//    return 0;
+//                    third:
+// int n;
+// cin>>n;
+// int i=1; int sum =0;
+// while (i<=n)
+// {
+//     sum=sum+i;
+//     i=i+1;
+// }
+// cout<< sum;
+// }
+//       FOURTH:
+int n; cin>>n;
+int i=1
+while (i<=n)
+{
+int j=1;
+while (j<=n)
+{
+ cout
+}
+
+}
+
+}
