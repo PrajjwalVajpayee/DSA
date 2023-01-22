256d35af617e549b4f3fb66c5617c78b43ff59dd
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2023-01-22 14:47:58 +0530

    lec5.cpp

---
+++ lec5.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    // int a,b=1;
+    // a=10;
+    // if (++a)
+    // {
+    //   cout<< b;
+    // }
+    //   else{
+    //   cout<< ++b;
+    // }
+    // int a=1,b=2;
+    // if (a-->0 && ++b>2)
+    // {
+    //    cout  <<"stage 1";
+    // }
+    // else{
+    //     cout<<"stage 2";
+    // }
+    // cout <<a <<b;
+    //           :fibonacci series:
+    // int n;
+    // cin>>n;
+    // int a=0,b=1;
+    // for (int i = 1; i <=n; i++)
+    // {
+    // int nextnumber= a+b;
+    // cout <<nextnumber<<"  ";
+    // a=b;
+    // b= nextnumber;
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "hey";
+    //     cout<<"hi";
+    //     continue;
+    //     cout<<"hello";
+
+    // }
+    // for (int i = 0; i <5; i++ )
+    // {
+    // for  (int j = i; j<=5; j++)
+    // {
+    //     cout<<i<<" "<<j;
+    // }
+
+    // }
+    // for (int i = 0; i <5; i++ )
+    // {
+    // for  (int j = i; j<=5; j++)
+    //  {
+    //   if (i+j ==10)
+    //   {
+    //     break;
+    //   }
+    //   cout<<i<<"  "<<j;
+    //  }
+    // }
+    int a, b;
+    cin >> a >> b;
+    cout << endl;
+    pow(a, b);
+    cout << endl;
+    cout << pow(a, b);
+}
